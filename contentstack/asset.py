04426621b2ced9d37104e68457e03d657cf7aff7
shaileshmishra <mshaileshr@gmail.com>
2019-08-13 13:53:49 +0530

    asset doc string added

--- contentstack/asset.py
+++ contentstack/asset.py
@@ -25,26 +25,32 @@
 class Asset:
 
     """
+
     Assets refer to all the media files (images, videos, PDFs, audio files, and so on) uploaded
     in your Contentstack repository for future use.
     These files can be attached and used in multiple entries.
     contentstack.asset
     ~~~~~~~~~~~~~~~~~~
+
     This module implements the Asset class.
     API Reference: https://www.contentstack.com/docs/apis/content-delivery-api/#assets
 
     """
 
-    def __init__(self, asset_uid: str = None):
-        self.__asset_uid = asset_uid
+    def __init__(self, uid=None):
 
+        """
+        :param uid: accepts asset_uid (optional) -> it is URI parameter required for fetch() Asset
+        :type : str
+
+        """
+        if uid is not None and isinstance(uid, str):
+            self.__asset_uid = uid
         self.__stack_instance = None
         self.__response = None
         self.__http_request = None
-
         self.__query_params = {}
         self.__stack_headers = {}
-
         self.__file_name = None
         self.__file_size = None
         self.__file_type = None
@@ -59,15 +65,31 @@ class Asset:
         self.__tags = None
 
     def instance(self, stack_instance):
+
+        """
+        :param stack_instance: contains stack information like config, headers and http_instance
+        :type stack_instance: contentstack.stack.Stack
+        :return: self
+        :rtype: Asset
+        """
         self.__stack_instance = stack_instance
         self.__stack_headers.update(self.__stack_instance.headers)
         self.__http_request = self.__stack_instance.http_request
 
-    def configure(self, response: dict):
-        # response is dictionary that check None and length
-        if response is not None and len(response) > 0:
-            # if response dictionary is Not None
+        return self
+
+    def configure(self, response):
+
+        """
+        :param response: response is Asset result
+        :type response: dict
+        :return: self
+        :rtype: Asset
+
+        """
+        if response is not None and isinstance(response, dict) and len(response) > 0:
             self.__response = response
+
             if 'filename' in self.__response:
                 self.__file_name = self.__response['filename']
             if 'file_size' in self.__response:
@@ -98,29 +120,39 @@ class Asset:
     def asset_uid(self):
 
         """
+        :return: asset_uid function returns asset_uid of the asset
+        :rtype: str
+
         [Example]
         uid = asset.asset_uid
-        :return: str of asset_uid
         """
+
         return self.__uid
 
     @property
     def filetype(self):
 
         """
+        :return: This function returns type of the asset file
+        :rtype: str
+
         [Example]
         file = asset.filetype
-        :return: str of filetype
+
         """
+
         return self.__file_type
 
     @property
     def filesize(self):
 
         """
+        :return: returns size of asset file
+        :rtype: str
+
         [Example]
         size = asset.file_size
-        :return: file_size
+
         """
         return self.__file_size
 
@@ -128,19 +160,26 @@ class Asset:
     def filename(self):
 
         """
+        :return: this function returns the name of the asset file.
+        :rtype: str
+
         [Example]
         filename = asset.filename
-        :return: filename
+
         """
+
         return self.__file_name
 
     @property
     def url(self):
 
         """
+        :return: this function returns the url of the Asset
+        :rtype: str
+
         [Example]
         file_url = asset.file_url
-        :return: file_url
+
         """
         return self.__file_url
 
@@ -148,9 +187,12 @@ class Asset:
     def to_json(self):
 
         """
+        :return: this function returns json response of Asset
+        :rtype: dict
+
         [Example]
         response = asset.to_json
-        :return: dict response
+
         """
         return self.__response
 
@@ -158,19 +200,25 @@ class Asset:
     def create_at(self):
 
         """
+        :return: this function returns the time when Asset is created.
+        :rtype:  str
+
         [Example]
         created_at = asset.created_at
-        :return: str created_at
+
         """
         return self.__created_at
 
     @property
-    def create_by(self):
+    def created_by(self):
 
         """
+        :return: this function returns the owner of the Asset
+        :rtype: str
+
         [Example]
         created_by = asset.created_by
-        :return: str created_by
+
         """
         return self.__created_by
 
@@ -178,19 +226,26 @@ class Asset:
     def update_at(self):
 
         """
+        :return: this method returns the time the Asset was updated.
+        :rtype: str
+
         [Example]
         updated_at = asset.updated_at
         :return: str updated_at
+
         """
         return self.__updated_at
 
     @property
-    def update_by(self):
+    def updated_by(self):
 
         """
+        :return: this function returns the time of Asset was updated
+        :rtype: str
+
         [Example]
         updated_by = asset.updated_by
-        :return: str updated_by
+
         """
         return self.__updated_by
 
@@ -198,9 +253,12 @@ class Asset:
     def tags(self):
 
         """
+        :return: this function returns the list of str
+        :rtype: list
+
         [Example]
         tags = asset.tags
-        :return: list tags
+
         """
         return self.__tags
 
@@ -208,93 +266,195 @@ class Asset:
     def get_version(self):
 
         """
+        :return: this returns the version of the asset
+        :rtype: int
+
         [Example]
-        tags = asset.tags
-        :return: list tags
+        version = asset.get_version
+
         """
         return self.__version
 
     @property
-    def dimension(self) -> tuple:
+    def dimension(self):
 
         """
+        :return: this function returns the dimension of the Asset
+        :rtype: dict
+
         [Example]
         dimension = asset.dimension
-        :return: tuple (height, width)
-        """
-        global width, height
-        if self.__dimension is not None and isinstance(self.__dimension, dict):
-            dim: dict = self.__dimension
-            height, width = dim.values()
-        return height, width
-
-    def headers(self, http_request, headers: dict):
 
         """
+        return self.__dimension
+
+    def headers(self, headers):
+
+        """
+        :param headers: headers is the stack headers for the stack authentication
+        :type headers: dict
+        :return: self
+        :rtype: Asset
+
         [Example]
-        headers = asset.headers(dict)
-        :return: self, that help to chaining the request.
+
+        asset_dict = {'api_key':'bltsomething', 'access_token':'someacceesstoken', 'environment':'development'}
+        asset = asset.headers(asset_dict)
 
         """
-        if http_request is not None:
-            self.__http_request = http_request
-        else:
-            raise Exception("Error! We Don't Have HTTP Instance")
-
-        if isinstance(headers, dict) and len(headers) > 0:
+        if headers is not None and isinstance(headers, dict) and len(headers) > 0:
             self.__stack_headers = headers.copy()
             if 'environment' in self.__stack_headers:
                 env = self.__stack_headers['environment']
-                self.__query_params["environment"] = env
+                self.__query_params['environment'] = env
                 self.__stack_headers.pop('environment', None)
             else:
                 raise Exception("Environment Can't Be None")
+        else:
+            Exception('Kindly provide a valid input')
 
         return self
 
-    def params(self, params: dict):
+    def params(self, params):
 
         """
-        add param method allows to add query param dictionary to the asset
-        :param params:
-        :return: self
+        :param params: param function allows to add/update asset query param dictionary.
+        :type params: dict
+        :return: instance of asset self
+        :rtype: Asset
+
+        [Example]
+
+        asset_param = {'key1':'paramOne', 'key2':'paramTwo', 'key3':'paramThree'}
+        asset = asset.params(asset_param)
+
         """
         if params is not None and isinstance(params, dict) and len(params) > 0:
             self.__query_params.update(params)
+
         return self
 
     def relative_urls(self):
-        self.__query_params['relative_urls'] = "true"
+
+        """
+        include the relative URLs of the assets in the response.
+        :return: instance of asset self
+        :rtype: Asset
+
+        [Example]
+        asset = asset.relative_urls()
+
+        """
+        self.__query_params['relative_urls'] = 'true'
+
         return self
 
     def version(self, version):
+
+        """
+        Note: If no version is mentioned, this request will retrieve the latest published version of the asset.
+        To retrieve a specific version, make use of the version parameter and keep the environment parameter blank.
+
+        Specify the version number of the asset that you wish to retrieve.
+        If the version is not specified, the details of the latest version will be retrieved.
+        To retrieve a specific version, keep the environment parameter blank.
+        Example: 1
+
+        :param version: retrieves asset of a specific version
+        :type version: int
+        :return: instance of asset self
+        :rtype: Asset
+
+        [Example]
+        asset = asset.version(1)
+
+        """
         if version is not None:
             self.__query_params['version'] = version
+
         return self
 
     def include_dimension(self):
+
+        """
+        include the dimensions (height and width) of the image in the response.
+        Supported image types: JPG, GIF, PNG, WebP, BMP, TIFF, SVG, and PSD.
+
+        :return: instance of asset
+        :rtype: Asset
+
+        [Example]
+        asset = asset.include_dimension()
+
+        """
         self.__query_params['include_dimension'] = "true"
         return self
 
     def remove_header(self, key):
-        if key is not None:
-            if key in self.__stack_headers:
-                self.__stack_headers.pop(key)
+
+        """
+        :param key: remove_header function takes key of header developer wants to remove.
+        :type key:
+        :return: instance of asset
+        :rtype: Asset
+
+        [Example]
+        asset = asset.remove_header('header_key')
+
+        """
+        if key is not None and key in self.__stack_headers:
+            self.__stack_headers.pop(key)
+        else:
+            raise Exception('Kindly provide valid input')
+
         return self
 
-    def set_uid(self, asset_uid: str):
-        if asset_uid is not None:
+    def set_uid(self, asset_uid):
+
+        """
+        Enter the unique ID of the asset of which you wish to retrieve the details.
+        Example: blt91af1e5af9c3639f
+
+        :param asset_uid:  asset_uid is unique ID of the asset of which you wish to retrieve the details
+        :type asset_uid:
+        :return: instance of asset
+        :rtype: Asset
+        """
+        if asset_uid is not None and isinstance(asset_uid, str):
             self.__asset_uid = asset_uid
+        else:
+            raise Exception('Kindly provide valid asset_uid')
+
+        return self
 
     def fetch_all(self):
+
+        """
+        Assets refer to all the media files (images, videos, PDFs, audio files, and so on)
+        uploaded in your Contentstack repository for future use. These files can be
+        attached and used in multiple entries.
+        Learn more about Assets [https://www.contentstack.com/docs/guide/content-management#working-with-assets].
+
+        :return: This call fetches the list of all the assets of a particular stack.
+        It also returns the content of each asset in the form of list[Assets].
+        :rtype: list[Asset]
+
+        """
         from contentstack import Config
         asset_url = Config().endpoint('assets')
         return self.__http_request.get_result(asset_url, self.__query_params, self.__stack_headers)
 
     def fetch(self):
+
+        """
+        This call fetches the latest version of a specific asset of a particular stack.
+        :return: It returns Asset on the basis of asset_uid
+        :rtype: Asset
+
+        """
         from contentstack import Config
         if self.__asset_uid is not None and len(self.__asset_uid) > 0:
             asset_url = '{}/{}'.format(Config().endpoint('assets'), self.__asset_uid)
-            return self.__http_request.get_result(asset_url, self.__query_params, self.__stack_headers)
         else:
             raise Exception("Kindly Provide Asset UID")
+        return self.__http_request.get_result(asset_url, self.__query_params, self.__stack_headers)
